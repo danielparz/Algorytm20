c238dab78dee3dd6e3ea0ec07314e4d8acb24e13
danielparz <dparzynski@gmail.com>
2022-11-28 10:46:04 +0100

    Add project files.

---
+++ Algorytm20/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Algorytm20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //List<int> input = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> output = new List<int>();
+
+            if (input.Count % 2 != 0)
+            {
+                for (int i = input.Count / 2; i > 0; i--)
+                {
+                    output.Add(input[(input.Count / 2) - i]);
+                    output.Add(input[(input.Count / 2) + i]);
+                }
+                output.Add(input.Count / 2 + 1);
+            }
+            else
+            {
+                for (int i = input.Count / 2; i > 0; i--)
+                {
+                    output.Add(input[(input.Count / 2) - i]);
+                    output.Add(input[(input.Count / 2) + i - 1]);
+                }
+            }
+
+            foreach (int i in output)
+                Console.Write($" {i}, ");
+        }
+    }
+}
